f97e9af00613a7eb793ed0feb112f7cf385c7804
Deb <deborah.tsao@gmail.com>
2016-11-27 07:16:31 -0800

    changed portfolio pieces, made site responsive

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(".jumper").on("click", function( e ) {
+    
+    e.preventDefault();
+
+    $("body, html").animate({ 
+        scrollTop: $( $(this).attr('href') ).offset().top 
+    }, 600);
+    
+});
